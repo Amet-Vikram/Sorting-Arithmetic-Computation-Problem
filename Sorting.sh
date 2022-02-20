d67c5af676af7f673a45b2e20146d711d51f6005
Amet-Vikram <amet1197@gmail.com>
2022-02-20 14:59:39 +0530

    UC-8

--- Sorting.sh
+++ Sorting.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 read -p "Enter the values of A B C: " a b c
 declare -A values
@@ -6,18 +6,28 @@ arr=[]
 n=0
 
 values['uc2']=$(($a+$b*$c))
-
 values['uc3']=$(($a*$b+$c))
-
 values['uc4']=$(($c+$a/$b))
-
 values['uc5']=$(($a%$b+$c))
-
-echo ${!values[@]}
 
 for i in ${!values[@]}
 do 
     arr[$n]=$((${values[$i]}))
     n=$(($n+1))
 done
-echo ${arr[@]} 
+echo "Array of All the solutions Before sorting: "${arr[@]} 
+
+for ((i=0;i<4;i++))
+do 
+	for ((j=0;j<4-i-1;j++))
+	do
+		if [ ${arr[$j]} -le ${arr[$j+1]} ] 
+		then 
+			temp=${arr[$j]}
+			arr[$j]=${arr[$j+1]}
+			arr[$j+1]=$temp
+		fi
+	done
+done
+
+echo "Array After the sorting (Descending Order): "${arr[@]}
