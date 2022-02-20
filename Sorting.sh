cb11bf163821eecb279b718edcc00c8112374eda
Amet-Vikram <amet1197@gmail.com>
2022-02-20 14:49:57 +0530

    UC-7

--- Sorting.sh
+++ Sorting.sh
@@ -1,14 +1,23 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 read -p "Enter the values of A B C: " a b c
 declare -A values
+arr=[]
+n=0
 
-values[0]=$(($a+$b*$c))
+values['uc2']=$(($a+$b*$c))
 
-values[1]=$(($a*$b+$c))
+values['uc3']=$(($a*$b+$c))
 
-values[2]=$(($c+$a/$b))
+values['uc4']=$(($c+$a/$b))
 
-values[3]=$(($a%$b+$c))
+values['uc5']=$(($a%$b+$c))
 
-echo ${values[@]}
+echo ${!values[@]}
+
+for i in ${!values[@]}
+do 
+    arr[$n]=$((${values[$i]}))
+    n=$(($n+1))
+done
+echo ${arr[@]} 
